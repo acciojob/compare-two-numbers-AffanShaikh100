49fd4650f2743d63292bc169f0669c32c84aef40
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-14 21:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function compare(a, b) {
+	let input1 =a; 
+	let input2 =b;
+	if(input1==input2){
+		alert("true");
+	}
+	else{
+		alert("false");
+	}
+}
+const a = prompt("Please First Input");
+const b = prompt("Please Second Input");
+compare(a,b);
